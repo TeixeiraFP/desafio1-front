f673d315729635b71f3964845c9335d6557ee3cb
topre <fpteixeira@outlook.com.br>
2021-11-10 19:41:54 -0300

    components  products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./Products.styles";
+
+function Products() {
+  return (
+    <S.Container>
+      <S.Title />
+      <S.Type />
+      <S.Id />
+    </S.Container>
+  );
+}
+
+export default Products;
---
+++ src/components/Products/Products.styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 200px;
+  width: 300px;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.h4`
+  font-size: 20px;
+`;
+export const Type = styled.h5`
+  font-size: 15px;
+`;
+export const Id = styled.p`
+  font-size: 15px;
+`;
